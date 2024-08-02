6013ac09025712e8ce89baea148dfbeca0202f8a
jyeong2525 <yhwan0226@gmail.com>
2024-08-02 17:33:24 +0900

    feat: API 예외 처리 BasicErrorController

---
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,30 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class ApiExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+
+        private String memberId;
+        private String name;
+    }
+}
